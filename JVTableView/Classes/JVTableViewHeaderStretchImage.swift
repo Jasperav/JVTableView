1b75119927635aa0fea5503cec8292f64d2f53df
Jasper <Jasperav@hotmail.com>
2019-02-17 23:49:32 +0100

    commit

--- JVTableView/Classes/JVTableViewHeaderStretchImage.swift
+++ JVTableView/Classes/JVTableViewHeaderStretchImage.swift
@@ -1,8 +1,13 @@
+import JVCurrentDevice
+
 public struct JVTableViewHeaderStretchImage {
+    
+    public static var defaultHeight: CGFloat = CurrentDevice.getValue(tablet: 300, phone: 300)
+    
     public let height: CGFloat
     public let image: UIImage?
     
-    public init(height: CGFloat, image: UIImage?) {
+    public init(height: CGFloat = JVTableViewHeaderStretchImage.defaultHeight, image: UIImage?) {
         self.height = height
         self.image = image
     }
