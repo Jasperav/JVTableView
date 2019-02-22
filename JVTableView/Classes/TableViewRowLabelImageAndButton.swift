24fb1919d3d8706f9b532fb9f1387614b6365a3d
Jasper <Jasperav@hotmail.com>
2019-02-22 22:31:05 +0100

    commit

--- JVTableView/Classes/TableViewRowLabelImageAndButton.swift
+++ JVTableView/Classes/TableViewRowLabelImageAndButton.swift
@@ -1,12 +1,12 @@
 import JVView
 
 open class TableViewRowLabelImageAndButton: TableViewRowLabelImage {
-    private let imageForButton: UIImage
     
+    private let display: Display
     var tappedRightButton: (() -> ())!
     
-    public init<T: RawRepresentable>(identifier: T, text: String = "", contentTypeJVLabel: ContentTypeJVLabelText = TableViewRowLabel.standardContentTypeJVLabel, imageLeft: UIImage? = nil, imageForButton: UIImage, tapped: (() -> ())? = nil, tappedRightButton: (() -> ())? = nil) {
-        self.imageForButton = imageForButton
+    public init<T: RawRepresentable>(identifier: T, text: String = "", contentTypeJVLabel: ContentTypeJVLabelText = TableViewRowLabel.standardContentTypeJVLabel, imageLeft: UIImage? = nil, displayRightButton: Display, tapped: (() -> ())? = nil, tappedRightButton: (() -> ())? = nil) {
+        self.display = displayRightButton
         self.tappedRightButton = tappedRightButton
         
         super.init(identifier: identifier, text: text, contentTypeJVLabel: contentTypeJVLabel, accessoryType: .none, image: imageLeft, tapped: tapped)
@@ -18,12 +18,24 @@ open class TableViewRowLabelImageAndButton: TableViewRowLabelImage {
         let _cell = cell as! TableViewCellLabelImageAndButton
         
         _cell.button.addTarget(self, action: #selector(_tappedButton), for: .touchUpInside)
-        _cell.button.setImage(imageForButton, for: .normal)
+        
+        switch display {
+        case .image(let image):
+            _cell.button.setTitle(nil, for: .normal)
+            _cell.button.setImage(image, for: .normal)
+        case .text(let text):
+            _cell.button.setTitle(text, for: .normal)
+            _cell.button.setImage(nil, for: .normal)
+        }
         
         super.configure(cell: cell)
     }
     
     @objc private func _tappedButton() {
         tappedRightButton()
+    }
+    
+    public enum Display {
+        case image(image: UIImage), text(text: String)
     }
 }
