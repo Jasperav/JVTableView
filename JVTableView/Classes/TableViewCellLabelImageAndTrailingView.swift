4ef4e992a653a2b7504c05d86dafbbd4e096fba2
Jasper <Jasperav@hotmail.com>
2019-06-28 18:34:24 +0200

    commit

--- JVTableView/Classes/TableViewCellLabelImageAndTrailingView.swift
+++ JVTableView/Classes/TableViewCellLabelImageAndTrailingView.swift
@@ -1,5 +1,5 @@
 import JVCurrentDevice
-import JVUIButtonExtensions
+import JVUIButton
 
 open class TableViewCellLabelImageAndButton: TableViewCellLabelImage {
     
@@ -11,7 +11,6 @@ open class TableViewCellLabelImageAndButton: TableViewCellLabelImage {
     
     open override func setup() {
         super.setup()
-        // TODO: Link JVUIButton
         button.stretchImage()
     }
 }
